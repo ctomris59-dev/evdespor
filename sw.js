c869cae43028420d7f10644b9319d1de20850b39
ctomris59-dev <ctomris59@gmail.com>
2026-09-17 12:21:14 +0300

    Refresh PWA cache for v5 training system

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'evde-guclen-v4';
-const ASSETS = ['./','./index.html','./styles.css','./app-v4.js','./manifest.webmanifest','./icon.svg'];
+const CACHE = 'evde-guclen-v5';
+const ASSETS = ['./','./index.html','./styles.css','./v5.css','./app-v5.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
