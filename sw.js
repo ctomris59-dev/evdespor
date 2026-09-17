7e5cc30522e0d38817d1701fe5a6d21ff84c62a5
ctomris59-dev <ctomris59@gmail.com>
2026-09-17 13:57:11 +0300

    Refresh PWA cache for abs-priority v6

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'evde-guclen-v5';
-const ASSETS = ['./','./index.html','./styles.css','./v5.css','./app-v5.js','./manifest.webmanifest','./icon.svg'];
+const CACHE = 'evde-guclen-v6';
+const ASSETS = ['./','./index.html','./styles.css','./v5.css','./v6.css','./app-v6.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
