dd23f558fbf11ed72735e49d56f9222f5bb3a772
ctomris59-dev <ctomris59@gmail.com>
2026-09-17 14:32:05 +0300

    Refresh PWA cache for mobile-first v7

--- sw.js
+++ sw.js
@@ -1,9 +1,5 @@
-const CACHE = 'evde-guclen-v6';
-const ASSETS = ['./','./index.html','./styles.css','./v5.css','./v6.css','./app-v6.js','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate', e => e.waitUntil(
-  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-));
-self.addEventListener('fetch', e => e.respondWith(
-  caches.match(e.request).then(r=>r || fetch(e.request))
-));
+const CACHE = 'evde-guclen-v7-mobile';
+const ASSETS = ['./','./index.html','./v7.css','./app-v7.js','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
