a7eef4b2ecea6c248951aa1cbf0561261c22539a
ctomris59-dev <ctomris59@gmail.com>
2026-09-18 13:52:18 +0300

    Refresh PWA cache for pelvic tilt demo fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'evde-guclen-v12-double-knee';
+const CACHE = 'evde-guclen-v12-pelvic-tilt-fix';
 const ASSETS = ['./','./index.html','./v7.css','./app-v7.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
