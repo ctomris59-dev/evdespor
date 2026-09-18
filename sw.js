68b9427dda67b321155f8b31c7650a06a6b9e128
ctomris59-dev <ctomris59@gmail.com>
2026-09-18 11:21:56 +0300

    Refresh PWA cache for hyperlordosis protocol

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'evde-guclen-v8-posture';
+const CACHE = 'evde-guclen-v9-hyperlordosis';
 const ASSETS = ['./','./index.html','./v7.css','./app-v7.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
