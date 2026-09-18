74a8513e272c6b13cec4756deb7361a29b25e03c
ctomris59-dev <ctomris59@gmail.com>
2026-09-18 13:58:35 +0300

    Refresh PWA cache for verified Vimeo exercise demos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'evde-guclen-v12-pelvic-tilt-fix';
+const CACHE = 'evde-guclen-v13-vimeo-demos';
 const ASSETS = ['./','./index.html','./v7.css','./app-v7.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
