dedfd67467a19f6d9f574d137b540238d0c087fa
ctomris59-dev <ctomris59@gmail.com>
2026-09-18 14:03:15 +0300

    Refresh PWA cache for Glute Bridge demo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'evde-guclen-v13-vimeo-demos';
+const CACHE = 'evde-guclen-v14-bridge-demo';
 const ASSETS = ['./','./index.html','./v7.css','./app-v7.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
